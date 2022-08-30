389d8f183a19e031e9a7417c6c63675255762630
Leandro Aparicio do Prado <lprado@lprsoft.com.br>
2022-08-29 21:59:52 -0300

    documentation improvement

--- docs/generate/build.ps1
+++ docs/generate/build.ps1
@@ -41,22 +41,38 @@ function Get-BasePath {
 
 $ItemAllUnits = '<li><a href="AllUnits.html">Units</a></li>'
 $ItemAllIdentifiers = '<li><a href="AllIdentifiers.html">Identifiers</a></li>'
-function Set-NavigationItemOrder {
+$ItemMenuRef = '</li></ul><hr><li>Reference'
+
+function Set-NavigationItemOrder{
+  param ($content, $navigationItem, $itemReference)
+  $content = $content.Replace($navigationItem, '')
+  $content = $content.Replace($itemReference, $navigationItem + $itemReference)
+  return $content
+}
+
+function Set-NavigationMenuOrder {
   param ($file)
   
   $Content = Get-Content -Path $file
 
-  $Content = $Content.Replace('<li><a href="install.html">Install</a></li>', '')
-  $Content = $Content.Replace($ItemAllUnits, '<li><a href="install.html">Install</a></li>' + $ItemAllUnits)
-
-  $Content = $Content.Replace('<li><a href="quickstart.html">Quick start</a></li>', '')
-  $Content = $Content.Replace($ItemAllUnits, '<li><a href="quickstart.html">Quick start</a></li>' + $ItemAllUnits)
-
-  $Content = $Content.Replace('<li><a href="advanced.html">Advanced features</a></li>', '')
-  $Content = $Content.Replace($ItemAllUnits, '<li><a href="advanced.html">Advanced features</a></li>' + $ItemAllUnits)
-
+  $Content = Set-NavigationItemOrder $Content '<li><a href="install.html">Install</a></li>' $ItemAllUnits
+  $Content = Set-NavigationItemOrder $Content '<li><a href="quickstart.html">Quick start</a></li>' $ItemAllUnits
+  $Content = Set-NavigationItemOrder $Content '<li><a href="advanced.html">Advanced features</a></li>' $ItemAllUnits
+  
   $Content = $Content.Replace($ItemAllUnits, '<hr><li>Reference<ul style="margin-left: 10px;">' + $ItemAllUnits)
   $Content = $Content.Replace($ItemAllIdentifiers, $ItemAllIdentifiers + '</li></ul>')
+
+  $Content = $Content.Replace('<hr><li>Reference', '<hr><li>Commands<ul style="margin-left: 10px;"></li></ul><hr><li>Reference')
+
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-add.html">Add</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-build.html">Build</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-clean.html">Clean</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-help.html">Help</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-install.html">Install</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-new.html">New</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-test.html">Test</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-version.html">Version</a></li>' $ItemMenuRef
+  $Content = Set-NavigationItemOrder $Content '<li><a href="command-watch.html">Watch</a></li>' $ItemMenuRef
 
   Set-Content -Path $file -Value $Content
 }
@@ -78,6 +94,15 @@ $DocArgs =
   "--additional="  + $BasePath + "docs\generate\install.txt " +
   "--additional="  + $BasePath + "docs\generate\quickstart.txt " +
   "--additional="  + $BasePath + "docs\generate\advanced.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-add.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-build.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-clean.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-help.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-install.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-new.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-test.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-version.txt " +
+  "--additional="  + $BasePath + "docs\generate\command-watch.txt " +
   $BasePath + "src\commands\*.pas " + 
   $BasePath + "src\utils\*.pas " + 
   $BasePath + "pasc.lpr "
@@ -87,5 +112,5 @@ Start-Process -FilePath "pasdoc" -ArgumentList $DocArgs -Wait -NoNewWindow
 $DocPath = Join-Path -Path $BasePath -ChildPath "docs\*.html"
 
 Get-ChildItem -Path $DocPath -Recurse | ForEach-Object {
-  Set-NavigationItemOrder -file $_
+  Set-NavigationMenuOrder -file $_
 }
